de84db42b52f3ca649e8a6d85c419b60d7aa004d
JoaoCleber3 <jc.rowletgrass@gmail.com>
2025-11-25 09:16:53 -0300

    DTO do pedido

---
+++ src/main/java/poo/PedidoDTO.java
@@ -0,0 +1,21 @@
+package poo;
+
+import java.util.List;
+import java.util.Map;
+
+public class PedidoDTO {
+    private int mesa;
+    private Map<Long, Integer> itens;
+public PedidoDTO(int mesa, Map<Long,Integer> itens){
+    this.mesa= mesa;
+    this.itens = itens;
+}
+
+    public int getMesa() {
+        return mesa;
+    }
+
+    public Map<Long, Integer> getItens() {
+        return itens;
+    }
+}
